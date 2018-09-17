6c7d1455ab0c14c3a6f146fa502504eeec66e14a
lhf <17686531379@qq.com;git config --global user.email 17686531379@qq.com>
2018-09-17 10:13:34 +0800

    ytcrm

--- common/config/bootstrap.php
+++ common/config/bootstrap.php
@@ -2,11 +2,4 @@
 Yii::setAlias('@common', dirname(__DIR__));
 Yii::setAlias('@frontend', dirname(dirname(__DIR__)) . '/frontend');
 Yii::setAlias('@backend', dirname(dirname(__DIR__)) . '/backend');
-Yii::setAlias('@console', dirname(dirname(__DIR__)) . '/console');
 Yii::setAlias('@api_backend', dirname(dirname(__DIR__)) . '/api_backend');
-Yii::setAlias('@sysadmin', dirname(dirname(__DIR__)) . '/sysadmin');
-Yii::setAlias('@superadmin', dirname(dirname(__DIR__)) . '/superadmin');
-Yii::setAlias('@staff', dirname(dirname(__DIR__)) . '/staff');
-Yii::setAlias('@api_staff', dirname(dirname(__DIR__)) . '/api_staff');
-Yii::setAlias('@agent', dirname(dirname(__DIR__)) . '/agent');
-Yii::setAlias('@batch', dirname(dirname(__DIR__)) . '/batch');
